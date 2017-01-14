81351b10cbb43c698b2991e1a2cf93695b475990
Massimiliano Ghilardi <massimiliano.ghilardi@gmail.com>
2017-01-14 15:57:06 +0100

    added full GPL v2.0 to COPYING. added explicit copyright to all sources and headers

--- config.h
+++ config.h
@@ -1,3 +1,28 @@
+/*
+ * detachtty - run long-lived processes non interactively in the background,
+ *             and reattach them to a terminal at later time
+ *
+ * Copyright (C) 2016-2017 Massimiliano Ghilardi
+ * Copyright (C) 2001-2005 Daniel Barlow
+ *
+ *     This program is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation; either version 2
+ *     of the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+#ifndef DETACHTTY_CONFIG_H
+#define DETACHTTY_CONFIG_H
+
 #include <netdb.h>
 #include <sys/poll.h>
 #include <sys/types.h>
@@ -44,3 +69,5 @@ int output_buffer_sendfd(int out_fd, int send_fd);
 extern FILE *log_fp;
 int logprintf(char *progname, char *format, ...);
 void bail(char *progname, char *format, ...);
+
+#endif /* DETACHTTY_CONFIG_H */
